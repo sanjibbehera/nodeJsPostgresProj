d5440231363aea1db34552d9f14106bdd0fb3c9f
sanjibbehera <51856169+sanjibbehera@users.noreply.github.com>
2019-12-05 05:12:50 +0530

    Initial Setup

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORTNO
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => res.render('index', {
+    title: 'Environment Management Dashboard', navbar_title: 'Welcome to Environment Configuration Managed System'
+  }));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
