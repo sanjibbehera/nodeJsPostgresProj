1344f4ca7343c3bd49a13fb5f36c6260cdafa9f5
sanjibbehera <51856169+sanjibbehera@users.noreply.github.com>
2019-12-05 22:03:19 +0530

    Update postgresdb_DDL_DML.sql

--- postgresdb_DDL_DML.sql
+++ postgresdb_DDL_DML.sql
@@ -5,3 +5,7 @@
   environment_name VARCHAR(20),
   application_name VARCHAR(50)
 );
+
+### Add Unique infdex to the above table.
+
+>> CREATE UNIQUE INDEX env_app_name_idx on env_appl (environment_name, application_name);
