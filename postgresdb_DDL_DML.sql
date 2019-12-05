6597bd36e642180cbcba5496c1963fb9eca113a3
sanjibbehera <51856169+sanjibbehera@users.noreply.github.com>
2019-12-05 23:56:21 +0530

    First Fetch from Postres DB.

--- postgresdb_DDL_DML.sql
+++ postgresdb_DDL_DML.sql
@@ -6,6 +6,6 @@
   application_name VARCHAR(50)
 );
 
-### Add Unique infdex to the above table.
+### Add Unique index to the above table.
 
 >> CREATE UNIQUE INDEX env_app_name_idx on env_appl (environment_name, application_name);
--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config();
+const cors = require('cors');
+const { pool } = require('./dbConnect');
 const app = express();
 var path = require('path');
 const port = process.env.PORTNO;
 
 app.set('view engine', 'pug');
 app.use("/public", express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.get('/', (req, res) => res.render('index', {
-    title: 'Environment Management Dashboard', navbar_title: 'Welcome to Environment Configuration Managed System'
+pool.query("select count(*) cntr, environment_name from env_appl group by environment_name", (error, results) => {
+    if (error) {
+      throw error
+    }
+    //console.log(results.rows);
+    envData = results.rows;
+  });
+
+app.get('/', (req, res) => res.render('index', {  
+  title: 'Environment Management Dashboard', navbar_title: 'Welcome to Environment Configuration Managed System',
+  data: JSON.stringify(envData)
   }));
 
 app.listen(port, () => console.log(`Env Mgmt Dashboard App listening on port ${port}!`))
